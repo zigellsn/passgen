fc0ef69e3130d23754e484902b0e737fd4e70994
Simon Zigelli <zigellsn@gmail.com>
2021-12-31 13:39:49 +0100

    New version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # passgen
 Python Password Generator
+
+## Parameters
+
+| Parameter        | Meaning                                              | Default value |
+|------------------|------------------------------------------------------|---------------|
+| -v, --version    | Shows a version message                              | n/a           |
+| -c, --count      | How many passwords should be generated?              | 1             |
+| -l, --length     | Number of characters in password                     | 10            |
+| -o, --obligatory | Must contain at least one of each chosen category    | false         |
+| --avoid-zero     | Avoid 0 and uppercase O                              | false         |
+| --avoid-one      | Avoid 1, lowercase l and uppercase I                 | false         |
+| --ascii          | Ascii letters (upper and lower case)                 | false         |
+| --lowercase      | Lowercase letters                                    | true          |
+| --uppercase      | Uppercase letters                                    | false         |
+| --digits         | Digits                                               | true          |
+| --punctuation    | Punctuation                                          | false         |
+| --sap            | SAP conform password (=> 'Do not start with ! or ?') | false         |
+| --custom         | File containing custom characters                    | none          |
--- passgen.py
+++ passgen.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# Copyright 2016 Simon Zigelli
+# Copyright 2016-2022 Simon Zigelli
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -14,28 +14,103 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import string
 from random import choice
-import sys
 
-print('Password generator')
-print('Copyright (c) 2016 Zigelli ENTERPRISES. All rights reserved.')
-print('\n')
-if len(sys.argv) > 1:
-    size = sys.argv[1]
-else:
-    size = 8
-    count = 1
-if len(sys.argv) > 2:
-    count = sys.argv[2]
-else:
-    count = 1
-try:
-    num = int(size)
-    k = int(count) 
-    for i in range(k):
-        passwort = ''.join([choice(string.ascii_lowercase + string.digits) for i in range(num)])
-        print (passwort)
-except:
-    print("Fehlerhafte Eingabe.")
-    print("Verwendung: 'python passgen.py <Laenge> <Anzahl>'") 
+
+def _contains_any(contains_string, contains_set):
+    return 1 in [c in contains_string for c in contains_set]
+
+
+def _passgen(password_length, count, ascii_letters, lowercase, uppercase, digits, punctuation, sap, obligatory,
+             avoid_zero, avoid_one, custom_chars_file):
+    custom_chars = None
+    if custom_chars_file is not None:
+        custom_chars = custom_chars_file.read()
+        characters = custom_chars
+    else:
+        characters = ""
+    if ascii_letters:
+        characters += string.ascii_letters
+    else:
+        if lowercase:
+            characters += string.ascii_lowercase
+        if uppercase:
+            characters += string.ascii_uppercase
+    if digits:
+        characters += string.digits
+    if punctuation:
+        characters += string.punctuation
+
+    std_password = False
+    if not ascii_letters and not lowercase and not uppercase and not digits and not punctuation and custom_chars == "":
+        characters = string.ascii_lowercase + string.digits
+        std_password = True
+
+    if avoid_zero:
+        characters.replace('0', '')
+        characters.replace('O', '')
+
+    if avoid_one:
+        characters.replace('1', '')
+        characters.replace('l', '')
+        characters.replace('I', '')
+
+    i = 0
+    while i < count:
+        passwort = ''.join([choice(characters) for i in range(password_length)])
+        if sap and (passwort[0] == "!" or passwort[0] == "?"):
+            continue
+        if obligatory:
+            if std_password:
+                if _contains_any(passwort, string.ascii_lowercase) and _contains_any(passwort, string.digits):
+                    print(passwort)
+                    i += 1
+                    continue
+            else:
+                if custom_chars is not None and not _contains_any(passwort, custom_chars):
+                    continue
+                if ascii_letters and not _contains_any(passwort, string.ascii_letters):
+                    continue
+                if lowercase and not _contains_any(passwort, string.ascii_lowercase):
+                    continue
+                if uppercase and not _contains_any(passwort, string.ascii_uppercase):
+                    continue
+                if digits and not _contains_any(passwort, string.digits):
+                    continue
+                if punctuation and not _contains_any(passwort, string.punctuation):
+                    continue
+                print(passwort)
+                i += 1
+        else:
+            print(passwort)
+            i += 1
+
+
+if __name__ == '__main__':
+    # initiate the parser
+    parser = argparse.ArgumentParser(prog='Password Generator')
+    parser.add_argument("-v", "--version", help="Show version", action="version",
+                        version="%(prog)s 2.0.0 - Copyright (c) 2016-2022 Zigelli ENTERPRISES. All rights reserved.")
+    parser.add_argument("-c", "--count", type=int, help="Password count", default=1)
+    parser.add_argument("-l", "--length", type=int, help="Password length", default=10)
+    parser.add_argument("-o", "--obligatory", help="Must contain at least one of each chosen category",
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("--avoid-zero", help="Avoid 0 and uppercase O", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--avoid-one", help="Avoid 1, lowercase l and uppercase I",
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("--ascii", help="Ascii letters (uppercase and lowercase)",
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("--lowercase", help="Lowercase", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--uppercase", help="Uppercase", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--digits", help="Digits", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--punctuation", help="Punctuation", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--sap", help="SAP conform password (=> 'Do not start with ! or ?')",
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("--custom", help="Custom characters file", type=argparse.FileType("r", encoding="UTF-8"),
+                        required=False)
+    args = parser.parse_args()
+
+    _passgen(args.length, args.count, args.ascii, args.lowercase, args.uppercase, args.digits, args.punctuation,
+             args.sap, args.obligatory, args.avoid_zero, args.avoid_one, args.custom)
